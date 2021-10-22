c307fbc3b1858fc1bb919b49e0a0d1e3729ef60b
Luis Javier Soriano Suarez <e.luissoriano@go.ugr.es>
2021-10-22 18:53:09 +0200

    Clase para almacenar datos de ingrediente closes #8

---
+++ app/ingrediente.js
@@ -0,0 +1,10 @@
+
+class Ingrediente{
+    constructor(nombre, calorias, grasas, hidratos, proteinas){
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.grasas = grasas;
+        this.hidratos = hidratos;
+        this.proteinas = proteinas;
+    }
+}
