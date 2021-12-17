facc0e804a69ca703adb53cf34d55929e03b5222
aleveji <alejandrovegajimenez@gmail.com>
2021-12-17 13:54:02 +0100

    Añade getter de proteinas y función maxProteinas #6
    Añade el getter de proteinas a la clase Ingrediente y añade
    una función que maximiza las proteinas minimizando las
    calorias y devuelve un conjunto de Ingredientes para obtener
    una receta ideal en función de un objetivo de proteinas y
    calorias.

--- app/ingrediente.js
+++ app/ingrediente.js
@@ -17,9 +17,29 @@ class Ingrediente {
         return this.calorias;
     }
 
+    getProteinas() {
+        return this.proteinas;
+    }
+
     toString() {
         return "Ingrediente -> " + this.nombre + " [Calorias: " + this.calorias + ", Grasas: " + this.grasas + ", Hidratos: " + this.hidratos + ", Proteinas: " + this.proteinas + "]";
     }
 }
 
+function maxProteinas(ingredientes, maxCalorias) {
+    ingredientes.sort(function(a,b){return (b.getProteinas()/b.getCalorias())-(a.getProteinas()/a.getCalorias())}); //ordena de mayor a menor segun la proporcion proteinas/calorias
+    
+    let aDevolver = [];
+    let calorias = 0;
+    for (let x of ingredientes) {
+        if ((calorias + x.getCalorias()) < maxCalorias) {
+            aDevolver.push(x.getNombre());
+            calorias += x.getCalorias();
+        }
+    }
+    
+    return aDevolver;
+}
+
 module.exports.Ingrediente = Ingrediente;
+module.exports.maxProteinas = maxProteinas;
